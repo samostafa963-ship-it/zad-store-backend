9950e53dc2e3f517d611df481cb7a22ec5f205a3
samostafa963-ship-it <samostafa963@gmail.com>
2026-06-28 16:30:15 +0300

    fix firebase admin init

--- firebase.js
+++ firebase.js
@@ -1,11 +1,10 @@
-const admin = require('firebase-admin');
+const firebaseAdmin = require('firebase-admin');
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+if (!firebaseAdmin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(serviceAccount)
   });
 }
 
-module.exports = admin;
+module.exports = firebaseAdmin;
